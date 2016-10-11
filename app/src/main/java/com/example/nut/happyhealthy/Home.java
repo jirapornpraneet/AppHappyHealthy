538b0782df0c291586fc633115a78171dc85800a
Jiraporn <Nut.jirapornpraneet@gmail.com>
2016-10-11 23:56:59 +0700

    OnCilckPage

--- app/src/main/java/com/example/nut/happyhealthy/Home.java
+++ app/src/main/java/com/example/nut/happyhealthy/Home.java
@@ -1,8 +1,10 @@
 package com.example.nut.happyhealthy;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends Activity {
 
@@ -10,5 +12,12 @@ public class Home extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+    }//Oncreate
+
+    public void ClickIntroHealthy(View view) {
+        startActivity(new Intent(Home.this,IntroHealthy.class));
     }
-}
+
+
+}//MainClass
