c9ff01a5af20c660f5bd4e14391025037650b6c2
Jiraporn <Nut.jirapornpraneet@gmail.com>
2016-10-12 21:58:45 +0700

    FinshinputUser IntentDisplayUser

--- app/src/main/java/com/example/nut/happyhealthy/DataUser.java
+++ app/src/main/java/com/example/nut/happyhealthy/DataUser.java
@@ -1,5 +1,6 @@
 package com.example.nut.happyhealthy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -79,6 +80,7 @@ public class DataUser extends AppCompatActivity {
         }else if (checkChoose()) {
             //Checked
             updateNewUserToServer();
+            startActivity(new Intent(DataUser.this,DisplayUser.class));
 
 
         }else {
