752f6e601d9a6d1c54a005ac649b452a9ae2ba16
Jiraporn <Nut.jirapornpraneet@gmail.com>
2016-10-20 22:35:11 +0700

    SplashScreen

--- app/src/main/java/com/example/nut/happyhealthy/SplashScreen.java
+++ app/src/main/java/com/example/nut/happyhealthy/SplashScreen.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -23,7 +26,24 @@ public class SplashScreen extends AppCompatActivity {
                 finish();
 
             }
-        }, 3000);
+        }, 4000);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView10);
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+
+        imageView.setAnimation(anim);
+
+        objHandler = new Handler();
+        objHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent objIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(objIntent);
+                finish();
+
+            }
+        }, 4000);
 
     }//oncreate
 }//MainClass
