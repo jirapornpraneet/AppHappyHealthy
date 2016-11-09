2cbfc15317e001421edec8b4d68eedbdf743138f
Jiraporn <Nut.jirapornpraneet@gmail.com>
2016-11-09 16:09:40 +0700

    datauser

--- app/src/main/java/com/example/nut/happyhealthy/History_Diabetes.java
+++ app/src/main/java/com/example/nut/happyhealthy/History_Diabetes.java
@@ -2,9 +2,12 @@ package com.example.nut.happyhealthy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,6 +19,9 @@ public class History_Diabetes extends AppCompatActivity {
     DiabetesTABLE dia;
     ArrayList<HashMap<String, String>> diabList;
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +30,10 @@ public class History_Diabetes extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listView);
 
         dia = new DiabetesTABLE(this);
+
+
+
+
 
         diabList = dia.getDiabetesList();
         adapter = new SimpleAdapter(History_Diabetes.this, diabList, R.layout.history_diabetes, new String[]{"id", "date", "time" ,"cos_1","cos_2"}, new int[]{R.id.id, R.id.date,R.id.time,R.id.cos_1,R.id.cos_2});
