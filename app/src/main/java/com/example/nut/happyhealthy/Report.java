1228ab134c0afa8d57bc67288acb2c728fdf6d33
Jiraporn <Nut.jirapornpraneet@gmail.com>
2016-11-13 12:04:54 +0700

    DesignIntro and Report

--- app/src/main/java/com/example/nut/happyhealthy/Report.java
+++ app/src/main/java/com/example/nut/happyhealthy/Report.java
@@ -1,7 +1,9 @@
 package com.example.nut.happyhealthy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Report extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class Report extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
-    }
-}
+    }//OnCreate
+    public void ClickReportBackHome(View view) {startActivity(new Intent(Report.this,MainActivity.class));}//ClickBackHome
+
+}//MainClass
