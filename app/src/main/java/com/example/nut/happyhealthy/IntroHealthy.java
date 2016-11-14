1cd47e5f548dc1fc4b60500be8c41a283234f31e
Jiraporn <Nut.jirapornpraneet@gmail.com>
2016-11-14 12:48:57 +0700

    PageAdepterIntroHealthy

--- app/src/main/java/com/example/nut/happyhealthy/IntroHealthy.java
+++ app/src/main/java/com/example/nut/happyhealthy/IntroHealthy.java
@@ -1,18 +1,19 @@
 package com.example.nut.happyhealthy;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
-public class IntroHealthy extends Activity {
+public class IntroHealthy extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_healthy);
-    }//OnCreate
 
-    public void ClickIntroBackHome(View view) {startActivity(new Intent(IntroHealthy.this,MainActivity.class));}//ClickBackHome
-}//MainClass
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewPageAndroid);
+        AndroidImageAdapter adapterView = new AndroidImageAdapter(this);
+        mViewPager.setAdapter(adapterView);
+
+    }
+}
