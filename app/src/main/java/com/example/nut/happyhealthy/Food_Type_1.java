d45eabe79facbf051e7bccc641653c99d2b0fe81
Jiraporn <Nut.jirapornpraneet@gmail.com>
2016-12-16 14:13:23 +0700

    FoodDetail

--- app/src/main/java/com/example/nut/happyhealthy/Food_Type_1.java
+++ app/src/main/java/com/example/nut/happyhealthy/Food_Type_1.java
@@ -18,7 +18,7 @@ public class Food_Type_1 extends AppCompatActivity {
     ListAdapter adapterFood1;
     FoodTABLE foodTABLE;
     ArrayList<HashMap<String, String>> foodList;
-    String Foodname, FoodUnit, FoodNetUnit;
+    String Foodname, FoodUnit, FoodNetUnit,FoodDetail;
     double FoodCalories, FoodNetweight, FoodProtein, FoodFat, FoodCarbohydrate, FoodSugars, FoodSodium;
     int FoodAmount, FoodId;
 
@@ -50,6 +50,7 @@ public class Food_Type_1 extends AppCompatActivity {
                 FoodCarbohydrate = Double.parseDouble(foodList.get(i).get("food_carbohydrate"));
                 FoodSugars = Double.parseDouble(foodList.get(i).get("food_sugars"));
                 FoodSodium = Double.parseDouble(foodList.get(i).get("food_sodium"));
+                FoodDetail = foodList.get(i).get("food_detail").toString();
 
                 //ส่งค่าไปอีกหน้าหนึ่ง putExtra
                 Intent intent2;
@@ -61,7 +62,7 @@ public class Food_Type_1 extends AppCompatActivity {
         });
 
         foodList = foodTABLE.getFoodList();
-        adapterFood1 = new SimpleAdapter(Food_Type_1.this, foodList, R.layout.food_1, new String[]{"food_name", "food_calories", "food_unit"}, new int[]{R.id.food_name, R.id.food_calories, R.id.food_unit});
+        adapterFood1 = new SimpleAdapter(Food_Type_1.this, foodList, R.layout.food_1, new String[]{"food_name", "food_calories", "food_unit","food_detail"}, new int[]{R.id.food_name, R.id.food_calories, R.id.food_unit , R.id.food_detail});
         listViewFood1.setAdapter(adapterFood1); //เป็นตัวที่เอาออกมาโชว์หน้าในlist
 
     }//onCreate
